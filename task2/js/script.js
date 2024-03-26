fd1435e477938a72418fb466e55b3187fee7c092
vetrimohan <159906012+vetrimohan@users.noreply.github.com>
2024-03-26 09:39:19 +0530

    Add files via upload

---
+++ task2/js/script.js
@@ -0,0 +1,9 @@
+//toggle menu button
+function toggleMenu(){
+    const menu=document.querySelector('.menu');
+    menu.classList.toggle('active')
+    const nav=document.querySelector('.nav')
+    menu.classList.toggle('active')
+    nav.classList.toggle('active')
+    
+}
